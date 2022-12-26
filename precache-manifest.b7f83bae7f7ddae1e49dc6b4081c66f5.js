f156d0b0a129d8df1db273d9a8c7f5c3f056d495
ryan <ryanma22303@gmail.com>
2022-12-26 18:25:05 -0500

    Updates

---
+++ precache-manifest.b7f83bae7f7ddae1e49dc6b4081c66f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed300ec50d4e7eda5d72",
+    "url": "/site/static/css/main.2db983eb.chunk.css"
+  },
+  {
+    "revision": "ed300ec50d4e7eda5d72",
+    "url": "/site/static/js/main.fdb9c1d7.chunk.js"
+  },
+  {
+    "revision": "14c83684653b1b55fe6d",
+    "url": "/site/static/js/runtime~main.de27f395.js"
+  },
+  {
+    "revision": "6156acc8c1eab84a29e8",
+    "url": "/site/static/css/2.990943bb.chunk.css"
+  },
+  {
+    "revision": "6156acc8c1eab84a29e8",
+    "url": "/site/static/js/2.cdd3ad0a.chunk.js"
+  },
+  {
+    "revision": "3e21cc8630845d9181d43e594f157b71",
+    "url": "/site/index.html"
+  }
+];
