1bdc4395959b21a4884b30b1744ac0dd18af31b9
ryan <ryanma22303@gmail.com>
2023-01-01 21:39:11 -0500

    add slide back

--- site/src/components/About.js
+++ site/src/components/About.js
@@ -11,29 +11,31 @@ function About() {
     return (
         <div className='about-container'>
             <img className='d-block w-100 about-bg' src={bg2} alt='blue' />
-            <div className='about-txt-container'>
-                <Container>
-                    <Row>
-                        <Col>
-                            <h1>
-                                Who am I?
-                            </h1>
-                            <p className='about-txt'>
-                                I am a 2nd-year CS student at Georgia Tech, specializing in artificial intelligence and systems+architecture
-                                <br />
-                                <br />
-                                I have experience in Java and Javascript, and in modern frameworks like React and Node
-                                <br />
-                                <br />
-                                I'm always looking to learn new technologies and work on cool stuff!
-                            </p>
-                        </Col>
-                        <Col>
-                            Dog Image
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+            <Slide left>
+                <div className='about-txt-container'>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <h1>
+                                    Who am I?
+                                </h1>
+                                <p className='about-txt'>
+                                    I am a 2nd-year CS student at Georgia Tech, specializing in artificial intelligence and systems+architecture
+                                    <br />
+                                    <br />
+                                    I have experience in Java and Javascript, and in modern frameworks like React and Node
+                                    <br />
+                                    <br />
+                                    I'm always looking to learn new technologies and work on cool stuff!
+                                </p>
+                            </Col>
+                            <Col>
+                                Dog Image
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </Slide>
         </div>
     );
 }
