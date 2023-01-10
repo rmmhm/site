d6c99c921f98c98bf911e040dbe4009be33197f3
ryan <ryanma22303@gmail.com>
2023-01-10 16:01:36 -0500

    finish foundation

--- site/src/containers/Resume.js
+++ site/src/containers/Resume.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Resume.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Button } from "react-bootstrap";
 import { Document, Page, pdfjs } from "react-pdf";
 import pdf from "../assets/2023resume.pdf";
 
@@ -8,15 +8,43 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 function Resume() {
   const [width, setWidth] = useState(1200);
+
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
 
+  function onButtonClick() {
+    fetch("../assets/2023resume.pdf").then((response) => {
+      response.blob().then((blob) => {
+        const fileURL = window.URL.createObjectURL(blob);
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "2023resume.pdf";
+        alink.click();
+      });
+    });
+  }
+
   return (
     <div className="resume-container">
-      <Container className="resume-content">
-        <h1>Resume</h1>
-        <Row>
+      <Container>
+        <Row
+          style={{
+            justifyContent: "center",
+            position: "relative",
+          }}
+        >
+          <Button
+            variant="light"
+            size="lg"
+            style={{ maxWidth: "300px" }}
+            onClick={onButtonClick}
+          >
+            Download Resume
+          </Button>
+        </Row>
+        <br />
+        <Row className="resume-content">
           <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
