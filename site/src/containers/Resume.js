570e77ab735efd2b6fbc47af74f68911885de5e7
ryan <ryanma22303@gmail.com>
2023-01-10 21:50:53 -0500

    resume

--- site/src/containers/Resume.js
+++ site/src/containers/Resume.js
@@ -8,28 +8,9 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 function Resume() {
   const [width, setWidth] = useState(1000);
-  function handleResize() {
-    if (window.innerWidth <= 768) {
-      setWidth(750);
-    } else {
-      setWidth(1000);
-    }
-  }
   useEffect(() => {
-    window.addEventListener("resize", handleResize);
-  });
-
-  function onButtonClick() {
-    fetch("../assets/2023resume.pdf").then((response) => {
-      response.blob().then((blob) => {
-        const fileURL = window.URL.createObjectURL(blob);
-        let alink = document.createElement("a");
-        alink.href = fileURL;
-        alink.download = "2023resume.pdf";
-        alink.click();
-      });
-    });
-  }
+    setWidth(window.innerWidth);
+  }, []);
 
   return (
     <div className="resume-container">
@@ -44,15 +25,16 @@ function Resume() {
             variant="light"
             size="lg"
             style={{ maxWidth: "300px" }}
-            onClick={onButtonClick}
+            href={pdf}
+            target="_blank"
           >
             Download Resume
           </Button>
         </Row>
         <br />
         <Row className="resume-content">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 768 ? 1.7 : 1.2} />
+          <Document file={pdf} className="d-flex justify-content-center resume">
+            <Page pageNumber={1} scale={width > 768 ? 1.7 : 0.6} />
           </Document>
         </Row>
       </Container>
