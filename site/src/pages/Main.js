e4e24190d9f665df90b2e7fe01e0c01b4c88a36b
ryan <ryanma22303@gmail.com>
2023-01-12 13:35:58 -0500

    fix navbar sticky when scroll

--- site/src/pages/Main.js
+++ site/src/pages/Main.js
@@ -4,26 +4,29 @@ import Header from "../containers/Header";
 import About from "../containers/About";
 import Projects from "../containers/Projects";
 import Resume from "../containers/Resume";
+import { useInView } from "react-intersection-observer";
+
 import "./Main.css";
 
 function Main() {
   const [formatState, setFormat] = useState("navbar");
-  function scrollHandler() {
-    console.log(window.scrollY);
-    if (window.scrollY >= 20) {
-      setFormat("sticky");
-    } else {
-      setFormat("navbar");
-    }
-  }
+  const [ref, inView] = useInView();
+
   useEffect(() => {
-    window.addEventListener("scroll", scrollHandler, true);
-  }, []);
+    console.log(inView);
+    if (inView) {
+      setFormat("navbar");
+    } else {
+      setFormat("sticky");
+    }
+  }, [inView]);
 
   return (
     <div className="app-container">
       <NavBar format={formatState} />
-      <Header />
+      <div ref={ref}>
+        <Header />
+      </div>
       <About />
       <Projects />
       <Resume />
